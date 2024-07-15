f31ced78c33c613d34692838268fed77f8504b9c
Ozgun OZ <ozgunn.oz@gmail.com>
2024-07-15 22:06:12 +0200

    refactor

--- src/main/java/com/homeofthewizard/VaultMojo.java
+++ src/main/java/com/homeofthewizard/VaultMojo.java
@@ -3,11 +3,13 @@ package com.homeofthewizard;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.springframework.vault.config.EnvironmentVaultConfiguration;
 import org.springframework.vault.core.VaultTemplate;
 
 import javax.inject.Inject;
 
 @Mojo(name = "run", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@SpringBootPlugin(EnvironmentVaultConfiguration.class)
 public class VaultMojo extends AbstractMojo {
 
     private final VaultTemplate vaultTemplate;
